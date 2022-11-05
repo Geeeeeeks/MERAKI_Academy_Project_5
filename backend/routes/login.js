8d5a9edf57bb7435b8c1e331fc6cdbd4c79c76b3
Alaa Ashour <109425267+AshourAlaa@users.noreply.github.com>
2022-11-05 13:17:07 +0200

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,9 +1,1 @@
-const express = require("express");
 
-const { login } = require("../controllers/login");
-
-const loginRouter = express.Router();
-
-loginRouter.post("/", login);
-
-module.exports = loginRouter;
