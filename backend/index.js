360882195715ec21c3a50478e87e53a8091e1844
Alaa Ashour <109425267+AshourAlaa@users.noreply.github.com>
2022-11-05 13:17:35 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,13 @@ const cors = require("cors");
 require("./models/db");
 
 //routers
-const usersRouter=require("./routes/users")
-const loginRouter=require("./routes/login")
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 // router middleware
-app.use("/register",usersRouter)
-app.use("/login",loginRouter)
 
 
 const PORT = process.env.PORT || 5000;
